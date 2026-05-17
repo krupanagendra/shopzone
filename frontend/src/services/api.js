14b53d44665e55c32ec86a537fe63f54d8faec20
krupanagendra <nkrupa232005@gmail.com>
2026-05-17 18:04:59 +0530

    frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
-const api = axios.create({ baseURL: `${API_URL}/api` });
+const api = axios.create({ baseURL: `${API_URL.replace(/\/$/, '')}/api` });
 
 api.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem('userInfo');
